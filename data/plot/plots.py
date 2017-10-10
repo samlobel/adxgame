92695b1e0cd532a1140e73de1c4ad58a45a969e2
eareyan <enrique3@gmail.com>
2017-10-10 08:32:52 -0400

    added value plot

--- data/plot/plots.py
+++ data/plot/plots.py
@@ -26,6 +26,8 @@ def get_data(dir_location, file_name):
     data.columns = ['game','agent','segment','reach','reward','wincount','wincost']
     data.loc[data['wincount'] >= data['reach'], 'activate'] = 1
     data.loc[data['wincount'] <  data['reach'], 'activate'] = 0
+    data.loc[data['wincount'] >= data['reach'], 'value'] = data['reward']
+    data.loc[data['wincount'] < data['reach'], 'value'] = 0
     data['effectivereward'] = data['reward'] * data['activate']
     data['profit'] = data['effectivereward'] - data['wincost']
     return data
@@ -36,7 +38,7 @@ def get_group_data(dir_location, agent1, agent2, agent1fix = True):
     agent2_data = []
     agent1_means = {}
     agent2_means = {}
-    columns = ['profit','activate','wincost']
+    columns = ['profit','activate','wincost', 'value']
     for column in columns: 
         agent1_means[column] = []
         agent2_means[column] = []
@@ -76,12 +78,13 @@ def plotmeans(xaxis, xlabel, ylabel, agent1, agent2, agent1_means, agent2_means,
 
 def plotGroup(dir_location, image_prefix, agent1, agent2, fixagent1):
     data = get_group_data(dir_location, agent1, agent2, fixagent1)    
-    fig, ax = pl.subplots(nrows=3, ncols=1, sharex=True)
+    fig, ax = pl.subplots(nrows=4, ncols=1, sharex=True)
     xaxis = [x for x in range(2,max_number_agents + 2)]
     xlabel = 'Number of agents'
-    plotmeans(xaxis, xlabel, 'Profit', agent1, agent2, data[0]['profit'], data[1]['profit'], ax[0], True)
-    plotmeans(xaxis, xlabel, 'Activation', agent1, agent2, data[0]['activate'], data[1]['activate'], ax[1], True)
+    plotmeans(xaxis, xlabel, 'Activation', agent1, agent2, data[0]['activate'], data[1]['activate'], ax[0], True)
+    plotmeans(xaxis, xlabel, 'Value', agent1, agent2, data[0]['value'], data[1]['value'], ax[1])
     plotmeans(xaxis, xlabel, 'Cost', agent1, agent2, data[0]['wincost'], data[1]['wincost'], ax[2])
+    plotmeans(xaxis, xlabel, 'Profit', agent1, agent2, data[0]['profit'], data[1]['profit'], ax[3], True)
     ax[0].set_title('Performance comparison, fixing one ' + (agent1 if fixagent1 else agent2) + ' agent')
     #pl.savefig('../plot/' + (agent1+'v'+agent2+'Version1' if fixagent1 else agent2+'v'+agent1+'Version1') + '.png')
     pl.savefig('../plot/' + image_prefix + (agent1+'v'+agent2+'Version2' if fixagent1 else agent2+'v'+agent1+'Version2') + '.png')
@@ -95,14 +98,33 @@ def produce_all_plots():
     #dir_location = '../results0.75-10k/'
     #image_prefix = 'demand-factor-0.75-10kimpressions-'
 
-    dir_location = '../results0.75-2k/'
-    image_prefix = 'demand-factor-0.75-2kimpressions-'
+    #dir_location = '../results0.75-2k/'
+    #image_prefix = 'demand-factor-0.75-2kimpressions-'
 
     #dir_location = '../results1.25-2k/'
     #image_prefix = 'demand-factor-1.25-2kimpressions-'
 
     #dir_location = '../results1.25-10k/'
     #image_prefix = 'demand-factor-1.25-10kimpressions-'
+
+    #dir_location = '../results0.75-2k-newreward/'
+    #image_prefix = 'demand-factor-0.75-2kimpressions-newreward-'
+
+    #dir_location = '../results1.25-2k-newreward/'
+    #image_prefix = 'demand-factor-1.25-2kimpressions-newreward-'
+
+    #dir_location = '../results0.75-10k-newreward/'
+    #image_prefix = 'demand-factor-0.75-10kimpressions-newreward-'
+
+    #dir_location = '../results1.25-10k-newreward/'
+    #image_prefix = 'demand-factor-1.25-10kimpressions-newreward-'
+    
+    #dir_location = '../results0.25-2k-newreward/'
+    #image_prefix = 'demand-factor-0.25-2kimpressions-newreward-'
+
+    dir_location = '../results3.0-2k-newreward/'
+    image_prefix = 'demand-factor-3.0-2kimpressions-newreward-'
+
 
     for x in combinations(['SI','WE','WF'], 2):
         for y in [True, False]:
