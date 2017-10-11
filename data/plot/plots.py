24bd7db7aa5ddc7663e38bc7f26044657c05d013
Enrique Areyan Viqueira <eareyanv@cs.brown.edu>
2017-10-11 15:51:35 -0400

    deviation graph plots and proportion plots

--- data/plot/plots.py
+++ data/plot/plots.py
@@ -11,10 +11,13 @@ import numpy as np
 import scipy as sp
 import scipy.stats
 from matplotlib import pyplot as pl
-from itertools import combinations
+from itertools import combinations, product
 
 
 def mean_confidence_interval(data, confidence=0.95):
+    """
+    Compute the confidence interval of the given data (array).
+    """
     a = 1.0*np.array(data)
     n = len(a)
     m, se = np.mean(a), scipy.stats.sem(a)
@@ -22,6 +25,13 @@ def mean_confidence_interval(data, confidence=0.95):
     return m, m-h, m+h
 
 def get_data(dir_location, file_name):
+    """
+    Reads the data produced by the game. 
+    The data contains the following columns:
+        game, agent, segment, reach, reward, wincount, wincost.
+    In principle this data is enough to compute any statistic
+    about the performance of an agent in the game.
+    """
     data = pd.read_csv(dir_location + file_name, header = None)
     data.columns = ['game','agent','segment','reach','reward','wincount','wincost']
     data.loc[data['wincount'] >= data['reach'], 'activate'] = 1
@@ -32,7 +42,13 @@ def get_data(dir_location, file_name):
     data['profit'] = data['effectivereward'] - data['wincost']
     return data
 
-def get_group_data(dir_location, agent1, agent2, agent1fix = True):
+def get_group_data(dir_location, agent1, agent2, agent1fix = True, max_number_agents = 20):
+    """
+    Computes aggregates of the data produced by the game.
+    Specifically, this function returns the means of 
+    profit, activate, wincost and value for each of the
+    agents received as a parameter.
+    """
     all_data = []
     agent1_data = []
     agent2_data = []
@@ -56,6 +72,10 @@ def get_group_data(dir_location, agent1, agent2, agent1fix = True):
     
 
 def plotmeans(xaxis, xlabel, ylabel, agent1, agent2, agent1_means, agent2_means, ax, addHorizontalLine = False):
+    """
+    Plots the data of one dimension (e.g., profit)
+    as produced by get_group_data()
+    """
     y_agent1 = [x for (x,y,z) in agent1_means]
     lb_agent1 = [y for (x,y,z) in agent1_means]
     ub_agent1 = [z for (x,y,z) in agent1_means]
@@ -76,7 +96,11 @@ def plotmeans(xaxis, xlabel, ylabel, agent1, agent2, agent1_means, agent2_means,
     ax.set_ylabel(ylabel)
 
 
-def plotGroup(dir_location, image_prefix, agent1, agent2, fixagent1):
+def plotGroup(dir_location, image_prefix, agent1, agent2, fixagent1, max_number_agents = 20):
+    """
+    Plots all relevant plots by repeatingly calling function 
+    plotmeans.
+    """
     data = get_group_data(dir_location, agent1, agent2, fixagent1)    
     fig, ax = pl.subplots(nrows=4, ncols=1, sharex=True)
     xaxis = [x for x in range(2,max_number_agents + 2)]
@@ -86,47 +110,40 @@ def plotGroup(dir_location, image_prefix, agent1, agent2, fixagent1):
     plotmeans(xaxis, xlabel, 'Cost', agent1, agent2, data[0]['wincost'], data[1]['wincost'], ax[2])
     plotmeans(xaxis, xlabel, 'Profit', agent1, agent2, data[0]['profit'], data[1]['profit'], ax[3], True)
     ax[0].set_title('Performance comparison, fixing one ' + (agent1 if fixagent1 else agent2) + ' agent')
-    #pl.savefig('../plot/' + (agent1+'v'+agent2+'Version1' if fixagent1 else agent2+'v'+agent1+'Version1') + '.png')
-    pl.savefig('../plot/' + image_prefix + (agent1+'v'+agent2+'Version2' if fixagent1 else agent2+'v'+agent1+'Version2') + '.png')
+    pl.savefig('../plot/' + image_prefix + (agent1+'v'+agent2 if fixagent1 else agent2+'v'+agent1) + '.png')
 
-# Plots varying the composition of the game, i.e., type and number of agents
-max_number_agents = 20
-def produce_all_plots():
-    #dir_location = '../results_Version2/'
-    #image_prefix = 'version2-'
-
-    #dir_location = '../results0.75-10k/'
-    #image_prefix = 'demand-factor-0.75-10kimpressions-'
-
-    #dir_location = '../results0.75-2k/'
-    #image_prefix = 'demand-factor-0.75-2kimpressions-'
-
-    #dir_location = '../results1.25-2k/'
-    #image_prefix = 'demand-factor-1.25-2kimpressions-'
-
-    #dir_location = '../results1.25-10k/'
-    #image_prefix = 'demand-factor-1.25-10kimpressions-'
-
-    #dir_location = '../results0.75-2k-newreward/'
-    #image_prefix = 'demand-factor-0.75-2kimpressions-newreward-'
-
-    #dir_location = '../results1.25-2k-newreward/'
-    #image_prefix = 'demand-factor-1.25-2kimpressions-newreward-'
-
-    #dir_location = '../results0.75-10k-newreward/'
-    #image_prefix = 'demand-factor-0.75-10kimpressions-newreward-'
-
-    #dir_location = '../results1.25-10k-newreward/'
-    #image_prefix = 'demand-factor-1.25-10kimpressions-newreward-'
-    
-    #dir_location = '../results0.25-2k-newreward/'
-    #image_prefix = 'demand-factor-0.25-2kimpressions-newreward-'
-
-    dir_location = '../results3.0-2k-newreward/'
-    image_prefix = 'demand-factor-3.0-2kimpressions-newreward-'
-
-
+def produce_all_plots(dir_location, image_prefix):
+    """
+    Plots varying the composition of the game, i.e., 
+    type and number of agents
+    """
     for x in combinations(['SI','WE','WF'], 2):
         for y in [True, False]:
             print('Graph: ' + str(x) + (', Fix Agent ' + str(x[0]) if y else ', Fix Agent ' + str(x[1])))
             plotGroup(dir_location, image_prefix, x[0], x[1], y)
+            
+            
+
+demand_factors = ['0.25','0.75','1.25','3.0']
+impressions = ['2k']
+    
+for (demand, supply) in product(demand_factors, impressions):
+    dir_location = '../results' + demand + '-' + supply + '-newreward/'
+    image_prefix = 'demand-factor-' + demand.replace('.','_') + '-' + supply + 'impressions-'
+    #produce_all_plots(dir_location, image_prefix)    
+    latex = """
+\\newpage
+\subsection*{Demand Factor """+demand+""", """+supply+""" impressions}
+\hspace*{-1in}
+\includegraphics[scale=0.65]{/home/eareyanv/workspace/adxgame/data/plot/""" + image_prefix + """SIvWE}
+\includegraphics[scale=0.65]{/home/eareyanv/workspace/adxgame/data/plot/""" + image_prefix + """SIvWF}
+
+\hspace*{-1in}
+\includegraphics[scale=0.65]{/home/eareyanv/workspace/adxgame/data/plot/""" + image_prefix + """WEvSI}
+\includegraphics[scale=0.65]{/home/eareyanv/workspace/adxgame/data/plot/""" + image_prefix + """WEvWF}
+
+\hspace*{-1in}
+\includegraphics[scale=0.65]{/home/eareyanv/workspace/adxgame/data/plot/""" + image_prefix + """WFvSI}
+\includegraphics[scale=0.65]{/home/eareyanv/workspace/adxgame/data/plot/""" + image_prefix + """WFvWE}
+    """
+    print(latex)
