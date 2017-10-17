f07cb09b83780d9546553577101e5127404a6169
eareyan <enrique3@gmail.com>
2017-10-17 19:55:56 -0400

    soft deviation graphs, produced with markov chain

--- data/plot/scripts/deviation_graphs.py
+++ data/plot/scripts/deviation_graphs.py
@@ -12,7 +12,7 @@ from basic_plots import get_data, mean_confidence_interval
 from itertools import product
 from progress import update_progress
 
-def produce_graph_data(dir_location, number_of_agents, reserve = 0, include_reserve = False):
+def produce_profile_data(dir_location, number_of_agents, reserve = 0, include_reserve = False):
     """
     Given the number of agents, produces a list of tuples:
         (Strategy profile, (Profit WE, Profit WF))
@@ -22,31 +22,34 @@ def produce_graph_data(dir_location, number_of_agents, reserve = 0, include_rese
     Note that this game is symmetric and thus, WEWEWF = WEWFWE= WFWEWE.
     For simplicity, we normalize by first indicating WEs and then WFs.
     """
-    graph_data = []
+    #profile_data = {}
+    profile_data = []
     for i in range(0,number_of_agents + 1):
         file_location = 'WEWF(' + str(number_of_agents - i) + '-' + str(i) + ')' + ('-r(' + str(reserve) + ')' if include_reserve else '')+'.csv'
         mix = get_data(dir_location, file_location)
         WEdata = mix[mix.agent.str.contains('WEAgent')]
         WFdata = mix[mix.agent.str.contains('WFAgent')]
-        graph_data.append(('WE' * (number_of_agents - i) + 'WF' * i, (WEdata.profit.mean(), WFdata.profit.mean())))
-    return graph_data
+        #profile_data['WE' * (number_of_agents - i) + 'WF' * i] = {'WE': {'n' : len(WEdata), 'mean': WEdata.profit.mean(), 'std': WEdata.profit.std()} , 'WF': {'n': len(WFdata), 'mean' : WFdata.profit.mean(), 'std': WFdata.profit.std()}}
+        #profile_data.append(('WE' * (number_of_agents - i) + 'WF' * i, (WEdata.profit.mean(), WFdata.profit.mean()), (WEdata.profit.std(), WFdata.profit.std()) , (len(WEdata), len(WFdata))))
+        profile_data.append(('WE' * (number_of_agents - i) + 'WF' * i, {'WE': {'n' : len(WEdata), 'mean': WEdata.profit.mean(), 'std': WEdata.profit.std()} , 'WF': {'n': len(WFdata), 'mean' : WFdata.profit.mean(), 'std': WFdata.profit.std()}}))
+    return profile_data
 
-def produce_deviation_graph(graph_data):
+def produce_deviation_graph(profile_data):
     """
     Given the data of the graph, produces a directed graph, DG,
-    whichi is an object of the library networkx.
+    which is an object of the library networkx.
     """
-    number_of_agents = len(graph_data)
+    number_of_profiles = len(profile_data)
     DG = nx.DiGraph()
-    DG.add_nodes_from([v[0] for v in graph_data])
-    for i in range(1, number_of_agents):
-        DG.add_node(graph_data[i][0])
+    DG.add_nodes_from([v[0] for v in profile_data])
+    for i in range(1, number_of_profiles):
+        DG.add_node(profile_data[i][0])
         # WE deviating to WF, i.e, is a WE making less than a WF?
-        if graph_data[i-1][1][0] < graph_data[i][1][1]:
-            DG.add_edge(graph_data[i-1][0], graph_data[i][0])
+        if profile_data[i-1][1]['WE']['mean'] < profile_data[i][1]['WF']['mean']:
+            DG.add_edge(profile_data[i-1][0], profile_data[i][0])
         # WF deviating to WE, i.e, is a WF making less than a WE?
-        if graph_data[i][1][1] < graph_data[i-1][1][0]:
-            DG.add_edge(graph_data[i][0], graph_data[i-1][0])
+        if profile_data[i][1]['WF']['mean'] < profile_data[i-1][1]['WE']['mean']:
+            DG.add_edge(profile_data[i][0], profile_data[i-1][0])
     return DG
 
 def get_pure_nash(DG):
@@ -55,18 +58,18 @@ def get_pure_nash(DG):
     graph is just a tree). This is a list of tuples (#WE,#WF)"""
     return [(i.count('WE'), i.count('WF')) for i in DG.nodes_iter() if DG.out_degree(i)==0]
 
-def plot_deviation_graph(demand_factor, impressions, graph_data, DG):
+def plot_deviation_graph(demand_factor, impressions, profile_data, DG):
     """
     Plots the deviation graph. This function produces
     a circular type of plot of the deviation graph.
     """
-    number_of_agents = len(graph_data)
+    number_of_profiles = len(profile_data)
     pos = nx.circular_layout(DG)
-    labels = dict((i, r'$WE^{' + str(i.count('WE')) + '}WF^{' + str(i.count('WF')) + '}$') for (i, v) in graph_data)
-    fig = plt.figure(3, figsize=(number_of_agents + 1,number_of_agents + 1))
+    labels = dict((i, r'$WE^{' + str(i.count('WE')) + '}WF^{' + str(i.count('WF')) + '}$') for (i, v) in profile_data)
+    fig = plt.figure(3, figsize=(number_of_profiles + 1,number_of_profiles + 1))
     nx.draw(DG, pos, labels = labels, node_size = 2500, font_color = 'white', node_color = 'blue')
-    plt.title('Deviation Graph, WE v WF, ' + str(number_of_agents - 1) + ' agents')
-    plt.savefig("deviationgraphWEWF-" + demand_factor.replace('.','_') + "-" + impressions + "-" + str(number_of_agents - 1)+".png")
+    plt.title('Deviation Graph, WE v WF, ' + str(number_of_profiles - 1) + ' agents')
+    plt.savefig("../deviationgraphWEWF-" + demand_factor.replace('.','_') + "-" + impressions + "-" + str(number_of_profiles - 1)+".png")
     plt.show()
     plt.close(fig)
         
@@ -95,10 +98,10 @@ def get_dict_of_pure_nash(demand_factor, impressions, dir_location):
     dict_of_pure_nash = {}
     for i in range(2, 21):
         print('Number of agents: ' + str(i))
-        graph_data = produce_graph_data(dir_location, i)
-        DG = produce_deviation_graph(graph_data)
+        profile_data = produce_profile_data(dir_location, i)
+        DG = produce_deviation_graph(profile_data)
         # Uncomment next line to bulk produce all graphs
-        #plot_deviation_graph(demand_factor, impressions, graph_data, DG)
+        #plot_deviation_graph(demand_factor, impressions, profile_data, DG)
         dict_of_pure_nash[i] = get_pure_nash(DG)
     return dict_of_pure_nash
 
@@ -116,6 +119,7 @@ def plot_all_proportion_pure_nash():
         dict_of_pure_nash = get_dict_of_pure_nash(demand, supply, dir_location)
         plot_proportion_pure_nash(demand, supply, dict_of_pure_nash)
 
+"""
 number_agents = 8
 demand_factor = '3.0'
 impressions = '2k'
@@ -123,7 +127,7 @@ dir_location = '../../results' + demand_factor + '-' + impressions + '-' + str(n
 profit_reserve_at_equilibirum = {}
 for r in range(0,131):
     update_progress(r / 130.0)
-    data = produce_graph_data(dir_location, number_agents, r , True)
+    data = produce_profile_data(dir_location, number_agents, r , True)
     DG = produce_deviation_graph(data)
     pure_nash = get_pure_nash(DG)
     list_profit = []
@@ -147,6 +151,6 @@ plt.show()
 # For inspection purposes, the next line gets the graph data for a 
 # given number of agents
 dir_location = '../../results0.25-2k-newreward/'
-graph_data = produce_graph_data(dir_location, number_of_agents = 3)
-DG = produce_deviation_graph(graph_data)
-plot_deviation_graph('0.25', '2k', graph_data, DG)
+profile_data = produce_profile_data(dir_location, number_of_agents = 3)
+DG = produce_deviation_graph(profile_data)
+plot_deviation_graph('0.25', '2k', profile_data, DG)"""
---
+++ data/plot/scripts/markov_chain.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 11:10:11 2017
+
+@author: enriqueareyan
+"""
+from deviation_graphs import produce_profile_data
+from discreteMarkovChain import markovChain
+import numpy as np
+import networkx as nx
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import write_dot
+
+
+def normal1BiggerNormal2(mean1, std1, n1, mean2, std2, n2):
+    """
+    Returns the probability that the sampling distribution
+    of the mean (i.e., normal distribution) defined
+    by mean1, std1, n1 is bigger than the sampling distribution
+    of the mean (i.e., normal distribution) defined
+    by mean2, std2, n2.
+    """
+    return 1.0 - norm.cdf(0, mean1 - mean2, (std1 / np.sqrt(std1)) + (std2 / np.sqrt(std2)))
+    
+def produce_soft_deviation_graph(profile_data):
+    """
+    Given the profile data, creates the directed, weighted deviation graph
+    """
+    number_of_profiles = len(profile_data)
+    DG = nx.DiGraph()
+    DG.add_nodes_from([v[0] for v in profile_data])
+    
+    # Boder cases first:
+    # Start, all WE deviate to WF
+    start_WE_data = profile_data[0][1]['WE']
+    move_WF_data =  profile_data[1][1]['WF']
+    start_WE_best = normal1BiggerNormal2(start_WE_data['mean'], start_WE_data['std'], start_WE_data['n'], move_WF_data['mean'], move_WF_data['std'], move_WF_data['n'])
+    DG.add_edge(profile_data[0][0], profile_data[0][0], label = float("{0:.2f}".format(start_WE_best)))
+    DG.add_edge(profile_data[0][0], profile_data[1][0], label = float("{0:.2f}".format(1 - start_WE_best)))
+    DG.add_weighted_edges_from([(profile_data[0][0], profile_data[0][0], start_WE_best), (profile_data[0][0], profile_data[1][0], 1 - start_WE_best)])
+    # End, all WF deviate to WE
+    end_WF_data = profile_data[number_of_profiles-1][1]['WF']
+    move_WE_data =  profile_data[number_of_profiles-2][1]['WE']
+    end_WF_best = normal1BiggerNormal2(end_WF_data['mean'], end_WF_data['std'], end_WF_data['n'], move_WE_data['mean'], move_WE_data['std'], move_WE_data['n'])
+    DG.add_edge(profile_data[number_of_profiles-1][0], profile_data[number_of_profiles-1][0], label = float("{0:.2f}".format(end_WF_best)))
+    DG.add_edge(profile_data[number_of_profiles-1][0], profile_data[number_of_profiles-2][0], label = float("{0:.2f}".format(1 - end_WF_best)))
+    DG.add_weighted_edges_from([(profile_data[number_of_profiles-1][0], profile_data[number_of_profiles-1][0], end_WF_best), (profile_data[number_of_profiles-1][0], profile_data[number_of_profiles-2][0], 1 - end_WF_best)])
+    for i in range(1, number_of_profiles - 1):
+        WE_data  = profile_data[i][1]['WE']
+        WF_data = profile_data[i][1]['WF']
+        backward_WE_data = profile_data[i-1][1]['WE']
+        forward_WF_data  = profile_data[i+1][1]['WF']
+        current_WF_best = normal1BiggerNormal2(WF_data['mean'], WF_data['std'], WF_data['n'], backward_WE_data['mean'], backward_WE_data['std'], backward_WE_data['n'])
+        deviate_to_WE = 1 - current_WF_best
+        current_WE_best = normal1BiggerNormal2(WE_data['mean'], WE_data['std'], WE_data['n'], forward_WF_data['mean'], forward_WF_data['std'], forward_WF_data['n'])
+        deviate_to_WF = 1 - current_WE_best
+        vector = [current_WF_best, current_WE_best, deviate_to_WE, deviate_to_WF]
+        normalization_constant = sum(vector)
+        normalized_vector = [x / normalization_constant for x in vector]
+        DG.add_edge(profile_data[i][0], profile_data[i][0], label = float("{0:.2f}".format(normalized_vector[0]+normalized_vector[1])))
+        DG.add_edge(profile_data[i][0], profile_data[i-1][0], label = float("{0:.2f}".format(normalized_vector[2])))
+        DG.add_edge(profile_data[i][0], profile_data[i+1][0], label = float("{0:.2f}".format(normalized_vector[3])))
+        DG.add_weighted_edges_from([(profile_data[i][0], profile_data[i][0], normalized_vector[0]+normalized_vector[1]), (profile_data[i][0], profile_data[i-1][0], normalized_vector[2]), (profile_data[i][0], profile_data[i+1][0], normalized_vector[3])])
+    return DG
+    
+def plot_soft_deviation_graph(DG):
+    """
+    Given a directed graph object DG with weighted edges, 
+    plots the soft deviation graph. To produce this graph,
+    first computes the steady state distribution.
+    """
+    #Compute the steady state distribution
+    P = nx.adjacency_matrix(DG)
+    mc = markovChain(P)
+    mc.computePi('linear') #We can also use 'power', 'krylov' or 'eigen'
+    steady_state = mc.pi
+    #print(steady_state)
+    #print(P.todense())
+    
+    map_steady_dist = {}
+    for i in range(0,len(profile_data)):
+        map_steady_dist[profile_data[i][0]] = steady_state[i]
+    
+    # Drawing edges weights
+    edge_labels = edge_labels=dict([((u,v,),float("{0:.2f}".format(d['weight']))) for u,v,d in DG.edges(data=True) if u!=v])
+    labels = dict((i, r'$WE^{' + str(i.count('WE')) + '}WF^{' + str(i.count('WF')) + '}$') for (i, v) in profile_data)
+    
+    pos=nx.circular_layout(DG)
+    number_of_profiles = DG.number_of_nodes()
+    fig = plt.figure(3, figsize=(number_of_profiles + 3,number_of_profiles + 3))
+    for node, prob in map_steady_dist.items():
+        print(node,prob)
+        nx.draw_networkx_nodes(DG, pos, nodelist=[node], alpha=prob, node_color = 'black', node_size = 5000)
+    nx.draw_networkx_edge_labels(DG, pos, edge_labels=edge_labels, label_pos = 0.3)
+    nx.draw_networkx_edges(DG, pos)
+    nx.draw_networkx_labels(DG, pos, labels, font_size=16, font_color='red')
+    #nx.draw(DG, pos,labels = labels, node_size = 2500, font_color = 'white')
+    #nx.draw(DG, pos)
+    plt.axis('off')
+    plt.savefig("../deviationgraphSoftWEWF-" + demand_factor.replace('.','_') + "-" + impressions + "-" + str(number_of_profiles)+".png")
+    plt.show()
+    plt.close(fig)
+
+# Testing one soft graph, manually
+number_agents = 2
+demand_factor = '3.0'
+impressions = '2k'
+dir_location = '../../results' + demand_factor + '-' + impressions + '-newreward/'
+profile_data = produce_profile_data(dir_location, number_agents)
+DG = produce_soft_deviation_graph(profile_data)
+#write_dot(DG, 'test.dot')
+plot_soft_deviation_graph(DG)
+
+# Bulk graph plotting
+for i in range(1,20):
+    profile_data = produce_profile_data(dir_location, i)
+    DG = produce_soft_deviation_graph(profile_data)
+    plot_soft_deviation_graph(DG)
