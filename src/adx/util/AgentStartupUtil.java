7a9abcb65a3963cad24c727eb68e9881291b2802
jakechanan <jake_chanan@brown.edu>
2020-03-28 10:51:26 -0500

    parameters can read config from resources

--- src/adx/util/AgentStartupUtil.java
+++ src/adx/util/AgentStartupUtil.java
@@ -1,5 +1,8 @@
 package adx.util;
 
+import java.time.Instant;
+import java.util.Map;
+
 import adx.agent.AgentLogic;
 import adx.agent.OfflineAgent;
 import adx.agent.OnlineAgent;
@@ -7,16 +10,21 @@ import adx.exceptions.AdXException;
 import adx.server.OfflineGameServerAbstract;
 
 public class AgentStartupUtil {
-	public static void startOnline(AgentLogic logic, String[] args) {
+	public static void startOnline(AgentLogic logic, String[] args, String name) {
 		String host = args[0];
 		int port = Integer.parseInt(args[1]);
-		String name = args[2];
+		
+		if (name == "") {
+			name = "agent_" + Instant.now().toString();
+		}
 		
 		new OnlineAgent(host, port, logic).connect(name);
 	}
 	
-	public static void testOffline(AgentLogic logic, OfflineGameServerAbstract server) throws AdXException {
-		new OfflineAgent(server, logic).connect("test_agent");
+	public static void testOffline(Map<String, AgentLogic> agents, OfflineGameServerAbstract server) throws AdXException {
+		for (Map.Entry<String, AgentLogic> ent : agents.entrySet()) {
+			new OfflineAgent(server, ent.getValue()).connect(ent.getKey());
+		}
 		server.runAdXGame();
 	}
 }
--- src/adx/util/Parameters.java
+++ src/adx/util/Parameters.java
@@ -2,6 +2,8 @@ package adx.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import org.ini4j.Ini;
 import org.ini4j.InvalidFileFormatException;
@@ -18,190 +20,200 @@ import adx.exceptions.AdXException;
  */
 public class Parameters {
 
-  // Number of seconds to wait for new players once the server starts.
-  private static int SECONDS_WAIT_PLAYERS;
+	// Number of seconds to wait for new players once the server starts.
+	private static int SECONDS_WAIT_PLAYERS;
 
-  // A flag to indicate whether the parameters have been read from the .ini file.
-  private static boolean initialized = false;
+	// A flag to indicate whether the parameters have been read from the .ini file.
+	private static boolean initialized = false;
 
-  // Allowable games.
-  public static final ImmutableSet<String> allowableGames = ImmutableSet.of("ONE-DAY-ONE-CAMPAIGN", "TWO-DAYS-ONE-CAMPAIGN", "TWO-DAYS-TWO-CAMPAIGNS");
+	// Allowable games.
+	public static final ImmutableSet<String> allowableGames = ImmutableSet.of("ONE-DAY-ONE-CAMPAIGN", "TWO-DAYS-ONE-CAMPAIGN", "TWO-DAYS-TWO-CAMPAIGNS");
 
-  // Number of real-time seconds for a simulated day.
-  private static int SECONDS_DURATION_DAY;
+	// Number of real-time seconds for a simulated day.
+	private static int SECONDS_DURATION_DAY;
 
-  // In case campaigns are auctioned, how many campaigns to auction.
-  private static int NUMBER_AUCTION_CAMPAINGS;
+	// In case campaigns are auctioned, how many campaigns to auction.
+	private static int NUMBER_AUCTION_CAMPAINGS;
 
-  // The rate at which quality learning will happen. Only relevant for games longer than 1 day.
-  private static double QUALITY_SCORE_LEARNING_RATE;
+	// The rate at which quality learning will happen. Only relevant for games longer than 1 day.
+	private static double QUALITY_SCORE_LEARNING_RATE;
 
-  // How many simulated days in a row.
-  private static int TOTAL_SIMULATED_GAMES;
+	// How many simulated days in a row.
+	private static int TOTAL_SIMULATED_GAMES;
 
-  // Total population size.
-  public static int POPULATION_SIZE;
+	// Total population size.
+	public static int POPULATION_SIZE;
 
-  // How many simulated days.
-  public static int TOTAL_SIMULATED_DAYS;
+	// How many simulated days.
+	public static int TOTAL_SIMULATED_DAYS;
 
-  // Campaigns can last several days. This list specifies the allowable number of days.
-  public static ImmutableList<Integer> CAMPAIGN_DURATIONS;
+	// Campaigns can last several days. This list specifies the allowable number of days.
+	public static ImmutableList<Integer> CAMPAIGN_DURATIONS;
 
-  /**
-   * Checks if the parameters have been initialized. Throws exception in the negative case.
-   * 
-   * @throws AdXException
-   */
-  public static void safeguard() throws AdXException {
-    if (!Parameters.initialized) {
-      throw new AdXException("Parameters not initialized");
-    }
-  }
+	/**
+	 * Checks if the parameters have been initialized. Throws exception in the negative case.
+	 * 
+	 * @throws AdXException
+	 */
+	public static void safeguard() throws AdXException {
+		if (!Parameters.initialized) {
+			throw new AdXException("Parameters not initialized");
+		}
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static int get_SECONDS_WAIT_PLAYERS() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.SECONDS_WAIT_PLAYERS;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static int get_SECONDS_WAIT_PLAYERS() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.SECONDS_WAIT_PLAYERS;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static int get_SECONDS_DURATION_DAY() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.SECONDS_DURATION_DAY;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static int get_SECONDS_DURATION_DAY() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.SECONDS_DURATION_DAY;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static int get_NUMBER_AUCTION_CAMPAINGS() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.NUMBER_AUCTION_CAMPAINGS;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static int get_NUMBER_AUCTION_CAMPAINGS() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.NUMBER_AUCTION_CAMPAINGS;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static double get_QUALITY_SCORE_LEARNING_RATE() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.QUALITY_SCORE_LEARNING_RATE;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static double get_QUALITY_SCORE_LEARNING_RATE() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.QUALITY_SCORE_LEARNING_RATE;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static int get_TOTAL_SIMULATED_GAMES() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.TOTAL_SIMULATED_GAMES;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static int get_TOTAL_SIMULATED_GAMES() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.TOTAL_SIMULATED_GAMES;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static int get_POPULATION_SIZE() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.POPULATION_SIZE;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static int get_POPULATION_SIZE() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.POPULATION_SIZE;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static int get_TOTAL_SIMULATED_DAYS() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.TOTAL_SIMULATED_DAYS;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static int get_TOTAL_SIMULATED_DAYS() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.TOTAL_SIMULATED_DAYS;
+	}
 
-  /**
-   * Getter.
-   * 
-   * @return
-   * @throws AdXException
-   */
-  public static ImmutableList<Integer> get_CAMPAIGN_DURATIONS() throws AdXException {
-    Parameters.safeguard();
-    return Parameters.CAMPAIGN_DURATIONS;
-  }
+	/**
+	 * Getter.
+	 * 
+	 * @return
+	 * @throws AdXException
+	 */
+	public static ImmutableList<Integer> get_CAMPAIGN_DURATIONS() throws AdXException {
+		Parameters.safeguard();
+		return Parameters.CAMPAIGN_DURATIONS;
+	}
 
-  /**
-   * Populate parameters from .ini file.
-   * 
-   * @param ini_file_location
-   * @throws InvalidFileFormatException
-   * @throws IOException
-   * @throws AdXException
-   */
-  public static void populateParameters(String ini_file_location, String type_of_game) throws InvalidFileFormatException, IOException, AdXException {
-    // Implements a singleton design pattern.
-    // The parameters can only be initialized once.
-    if (!Parameters.initialized) {
-      if (!Parameters.allowableGames.contains(type_of_game)) {
-        throw new AdXException("The type of game " + type_of_game + " is not among the valid options " + Parameters.allowableGames);
-      }
-      // Read the .ini file.
-      Ini ini = new Ini(new File(ini_file_location));
+	/**
+	 * Populate parameters from .ini file.
+	 * 
+	 * @param ini_file_location
+	 * @throws InvalidFileFormatException
+	 * @throws IOException
+	 * @throws AdXException
+	 */
+	public static void populateParameters(String ini_file_location, String type_of_game) throws InvalidFileFormatException, IOException, AdXException {
+		// Implements a singleton design pattern.
+		// The parameters can only be initialized once.
+		if (!Parameters.initialized) {
+			if (!Parameters.allowableGames.contains(type_of_game)) {
+				throw new AdXException("The type of game " + type_of_game + " is not among the valid options " + Parameters.allowableGames);
+			}
+			// Read the .ini file.
+			InputStream strm = Parameters.class.getClassLoader().getResourceAsStream(ini_file_location);
+			Ini ini;
+			try {
+				if (strm != null) {
+					ini = new Ini(strm);
+				} else {
+					ini = new Ini(new File(ini_file_location));
+				}
+			} catch (Exception e) {
+				ini = new Ini(new File(ini_file_location));
+			}
 
-      // Global parameters
-      Parameters.SECONDS_WAIT_PLAYERS = Integer.parseInt(ini.get("PARAMETERS").get("SECONDS_WAIT_PLAYERS"));
-      Parameters.SECONDS_DURATION_DAY = Integer.parseInt(ini.get("PARAMETERS").get("SECONDS_DURATION_DAY"));
-      Parameters.NUMBER_AUCTION_CAMPAINGS = Integer.parseInt(ini.get("PARAMETERS").get("NUMBER_AUCTION_CAMPAINGS"));
-      Parameters.QUALITY_SCORE_LEARNING_RATE = Double.parseDouble(ini.get("PARAMETERS").get("QUALITY_SCORE_LEARNING_RATE"));
-      Parameters.TOTAL_SIMULATED_GAMES = Integer.parseInt(ini.get("PARAMETERS").get("TOTAL_SIMULATED_GAMES"));
-      Parameters.POPULATION_SIZE = Integer.parseInt(ini.get("PARAMETERS").get("POPULATION_SIZE"));
+			// Global parameters
+			Parameters.SECONDS_WAIT_PLAYERS = Integer.parseInt(ini.get("PARAMETERS").get("SECONDS_WAIT_PLAYERS"));
+			Parameters.SECONDS_DURATION_DAY = Integer.parseInt(ini.get("PARAMETERS").get("SECONDS_DURATION_DAY"));
+			Parameters.NUMBER_AUCTION_CAMPAINGS = Integer.parseInt(ini.get("PARAMETERS").get("NUMBER_AUCTION_CAMPAINGS"));
+			Parameters.QUALITY_SCORE_LEARNING_RATE = Double.parseDouble(ini.get("PARAMETERS").get("QUALITY_SCORE_LEARNING_RATE"));
+			Parameters.TOTAL_SIMULATED_GAMES = Integer.parseInt(ini.get("PARAMETERS").get("TOTAL_SIMULATED_GAMES"));
+			Parameters.POPULATION_SIZE = Integer.parseInt(ini.get("PARAMETERS").get("POPULATION_SIZE"));
 
-      // Game-specific parameters.
-      Parameters.TOTAL_SIMULATED_DAYS = Integer.parseInt(ini.get(type_of_game).get("TOTAL_SIMULATED_DAYS"));
-      Parameters.CAMPAIGN_DURATIONS = ImmutableList.of(Integer.parseInt(ini.get(type_of_game).get("CAMPAIGN_DURATIONS")));
+			// Game-specific parameters.
+			Parameters.TOTAL_SIMULATED_DAYS = Integer.parseInt(ini.get(type_of_game).get("TOTAL_SIMULATED_DAYS"));
+			Parameters.CAMPAIGN_DURATIONS = ImmutableList.of(Integer.parseInt(ini.get(type_of_game).get("CAMPAIGN_DURATIONS")));
 
-      // Mark the parameters as initialized, thus implementing the singleton pattern.
-      Parameters.initialized = true;
-    } else {
-      throw new AdXException("Parameters already initialized.");
-    }
-  }
+			// Mark the parameters as initialized, thus implementing the singleton pattern.
+			Parameters.initialized = true;
+		} else {
+			throw new AdXException("Parameters already initialized.");
+		}
+	}
 
-  public static void main(String[] args) throws InvalidFileFormatException, IOException, AdXException {
-    // Parameters.populateParameters("config/config.ini", "ONE-DAY-ONE-CAMPAIGN");
-    // Parameters.populateParameters("config/config.ini", "TWO-DAYS-ONE-CAMPAIGN");
-    // Parameters.populateParameters("config/config.ini", "TWO-DAYS-TWO-CAMPAIGNS");
-    Parameters.populateParameters(args[0], args[1]);
-    int s = Parameters.get_SECONDS_DURATION_DAY();
-    int c = Parameters.get_NUMBER_AUCTION_CAMPAINGS();
-    double d = Parameters.get_QUALITY_SCORE_LEARNING_RATE();
-    int n = Parameters.get_TOTAL_SIMULATED_GAMES();
-    int p = Parameters.get_POPULATION_SIZE();
-    int days = Parameters.get_TOTAL_SIMULATED_DAYS();
-    ImmutableList<Integer> camp = Parameters.get_CAMPAIGN_DURATIONS();
-    System.out.println(s);
-    System.out.println(c);
-    System.out.println(d);
-    System.out.println(n);
-    System.out.println(p);
-    System.out.println(days);
-    System.out.println(camp);
-  }
+	public static void main(String[] args) throws InvalidFileFormatException, IOException, AdXException {
+		// Parameters.populateParameters("config/config.ini", "ONE-DAY-ONE-CAMPAIGN");
+		// Parameters.populateParameters("config/config.ini", "TWO-DAYS-ONE-CAMPAIGN");
+		// Parameters.populateParameters("config/config.ini", "TWO-DAYS-TWO-CAMPAIGNS");
+		Parameters.populateParameters(args[0], args[1]);
+		int s = Parameters.get_SECONDS_DURATION_DAY();
+		int c = Parameters.get_NUMBER_AUCTION_CAMPAINGS();
+		double d = Parameters.get_QUALITY_SCORE_LEARNING_RATE();
+		int n = Parameters.get_TOTAL_SIMULATED_GAMES();
+		int p = Parameters.get_POPULATION_SIZE();
+		int days = Parameters.get_TOTAL_SIMULATED_DAYS();
+		ImmutableList<Integer> camp = Parameters.get_CAMPAIGN_DURATIONS();
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(n);
+		System.out.println(p);
+		System.out.println(days);
+		System.out.println(camp);
+	}
 }
